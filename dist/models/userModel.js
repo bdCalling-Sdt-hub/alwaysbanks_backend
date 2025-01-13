d310c4c13c8cef917d37c613b642baf8eccd37f2
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-01-13 21:32:39 +0600

    removing docker-compose

---
+++ dist/models/userModel.js
@@ -0,0 +1,178 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const enum_1 = require("../shared/enum");
+const userSchema = new mongoose_1.Schema({
+    auth: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: "Auth",
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+    },
+    dateOfBirth: {
+        type: String,
+    },
+    gender: {
+        type: String,
+        enum: enum_1.Gender,
+    },
+    avatar: {
+        type: String,
+    },
+    licensePhoto: {
+        type: String,
+        required: true,
+    },
+    isResturentOwner: {
+        type: Boolean,
+        default: false,
+    },
+    resturentName: {
+        type: String,
+    },
+    schedule: {
+        type: [
+            {
+                day: {
+                    type: String,
+                    required: true,
+                },
+                isActive: {
+                    type: Boolean,
+                    required: true,
+                },
+                startAt: {
+                    type: String,
+                },
+                endAt: {
+                    type: String,
+                },
+            },
+        ],
+        required: false,
+    },
+    reviews: {
+        type: [
+            {
+                user: {
+                    type: mongoose_1.Schema.Types.ObjectId,
+                    ref: "User",
+                    required: true,
+                },
+                name: {
+                    type: String,
+                    required: true,
+                },
+                avatar: {
+                    type: String,
+                },
+                rating: {
+                    type: Number,
+                    required: true,
+                    min: [1, "Star rating must be at least 1"],
+                    max: [5, "Star rating must not exceed 5"],
+                },
+                comment: {
+                    type: String,
+                },
+                createdAt: {
+                    type: Date,
+                    required: true,
+                },
+                updatedAt: {
+                    type: Date,
+                    required: true,
+                },
+            },
+        ],
+    },
+    stripeAccountId: {
+        type: String,
+    },
+    stripeAccoutStatus: {
+        type: Boolean,
+    },
+    tickets: {
+        type: [
+            {
+                event: {
+                    type: mongoose_1.Schema.Types.ObjectId,
+                    ref: "Event",
+                    required: true,
+                },
+                title: {
+                    type: String,
+                    required: true,
+                },
+                description: {
+                    type: String,
+                    required: true,
+                },
+                location: {
+                    type: String,
+                    required: true,
+                },
+                date: {
+                    type: Date,
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                },
+                customId: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        required: false,
+    },
+    guests: {
+        type: [
+            {
+                user: {
+                    type: mongoose_1.Schema.Types.ObjectId,
+                    ref: "User",
+                    required: true,
+                },
+                event: {
+                    type: mongoose_1.Schema.Types.ObjectId,
+                    ref: "Event",
+                    required: true,
+                },
+                name: {
+                    type: String,
+                    required: true,
+                },
+                avatar: {
+                    type: String,
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                },
+                eventTitle: {
+                    type: String,
+                    required: true,
+                },
+                location: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+    },
+});
+const User = (0, mongoose_1.model)("User", userSchema);
+exports.default = User;
---
+++ dist/shared/enum.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Gender = exports.Role = void 0;
+var Role;
+(function (Role) {
+    Role["GUEST"] = "GUEST";
+    Role["HOST"] = "HOST";
+    Role["DJ"] = "DJ";
+    Role["BARTENDER"] = "BARTENDER";
+    Role["BOTTLEGIRL"] = "BOTTLEGIRL";
+    Role["ADMIN"] = "ADMIN";
+})(Role || (exports.Role = Role = {}));
+var Gender;
+(function (Gender) {
+    Gender["MALE"] = "MALE";
+    Gender["FEMALE"] = "FEMALE";
+})(Gender || (exports.Gender = Gender = {}));
